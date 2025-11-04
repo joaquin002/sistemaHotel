93d308b5898a78ef25737dcf2215df09f2583ae8
joaquin002 <joaquin.manuel.olivera123@gmail.com>
2025-11-04 10:19:52 -0300

    mod administracion

--- joaquin olivera/src/Clase/Administracion.java
+++ joaquin olivera/src/Clase/Administracion.java
@@ -3,53 +3,18 @@ package Clase;
 import Enums.CantidadPersonas;
 import Enums.TipoHabitacion;
 
+import java.util.HashSet;
+
 public class Administracion extends Usuario {
-
-    private Generica<Habitacion> catalogo; //atributo habitaciones
-    // definir próximos atributos
-
-    public Administracion(String nombreUsuario, String contrasenia, Generica<Habitacion> catalogo) {
-        super(nombreUsuario, contrasenia);
-        this.catalogo = new Generica<>();
-    }
-    //modificar constructor con próximos atributos
+    private HashSet<Hotel>hoteles;
 
     //constructor para usuario
     public Administracion(String nombreUsuario, String contrasenia) {
         super(nombreUsuario, contrasenia);
-        this.catalogo = new Generica<>();
+        this.hoteles = new HashSet<>();
     }
 
-    //getters
-    public Generica<Habitacion> getCatalogo() {
-        return catalogo;
+    public HashSet<Hotel> getHoteles() {
+        return hoteles;
     }
-
-
-    /// metodos admin
-    // metodos de habitaciones:
-    public boolean agregarHabitacion (int precio, String descripcion, TipoHabitacion tipo, CantidadPersonas cantPersonas, String servicios, boolean estado){ //este metodo es de prueba
-        Habitacion H = new Habitacion(precio, descripcion, tipo, cantPersonas, servicios, estado);
-        return this.catalogo.agregarEnListado(H);
-    }
-    public boolean borrarHabitacion(int idBuscado){
-        return this.catalogo.eliminarHabitacion(idBuscado);
-    }
-    public String muestraHabitacion(int idBuscado){
-        String info = this.catalogo.mostrarHabitacion(idBuscado);
-        return info;
-    }
-    //faltan mas
-
-    //los generales
-    public boolean agregarHabitacion2(int precio, String descripcion, TipoHabitacion tipo, CantidadPersonas cantPersonas, String servicios, boolean estado){
-        Habitacion h1=new Habitacion(precio, descripcion, tipo, cantPersonas, servicios, estado);
-        return catalogo.agregar(h1);
-    }
-
-
-    public String verHabitacion(){
-        return catalogo.mostrarTodo();
-    }
-
 }
